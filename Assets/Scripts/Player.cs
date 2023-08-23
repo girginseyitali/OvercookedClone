ac3490b73cd94d35005a5c8ae664e7b48df330ec
seyitaligirgin <girginseyitali@gmail.com>
2023-08-23 14:31:16 +0300

    Updated Input System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,27 +8,13 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotationSpeed = 5f;
 
+    [SerializeField] private GameInput _gameInput;
+
     private bool isWalking;
     
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1f;
-        }if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1f;
-        }if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1f;
-        }if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1f;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * (moveSpeed * Time.deltaTime);
